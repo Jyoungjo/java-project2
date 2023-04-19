42cf0774945fb8835a090315b64b41573ca54042
Jyoungjo <zimbote0411@gmail.com>
2023-04-19 14:23:25 +0900

    feat: JavaInput, JavaInputTest 추가

---
+++ src/main/java/com/example/week1/JavaInputTest.java
@@ -0,0 +1,11 @@
+package com.example.week1;
+
+import java.io.IOException;
+
+public class JavaInputTest {
+    public static void main(String[] args) throws IOException {
+        JavaInput javaInput = new JavaInput();
+        javaInput.readTwoChars();
+    }
+}
+
