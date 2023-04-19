5b61266ca63f3de04c7e12cd8c91c92dfb3fc6df
Jyoungjo <zimbote0411@gmail.com>
2023-04-19 15:29:06 +0900

    feat: ScannerEx, ScannerExTest 추가

--- src/main/java/com/example/week1/BufferedReaderEx.java
+++ src/main/java/com/example/week1/BufferedReaderEx.java
@@ -12,4 +12,11 @@ public class BufferedReaderEx {
         System.out.println("line1 = " + br.readLine());
         System.out.println("line2 = " + br.readLine());
     }
+
+    // 숫자 2개 더하기 (X)
+    public void plusTwoNumbers() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        System.out.println(br.read() + br.read());
+    }
 }
---
+++ src/main/java/com/example/week1/ScannerExTest.java
@@ -0,0 +1,10 @@
+package com.example.week1;
+
+import java.util.Scanner;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus();
+    }
+}
