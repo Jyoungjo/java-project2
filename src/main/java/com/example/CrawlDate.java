1b465e5306d4e389f6212914d03bff7decf67715
Jyoungjo <zimbote0411@gmail.com>
2023-04-19 13:10:05 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/example/CrawlDate.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+    // alt + insert
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
