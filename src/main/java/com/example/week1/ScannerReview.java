0f10870573cf72acc767b6f9ba27bd494a23d3e8
Jyoungjo <zimbote0411@gmail.com>
2023-04-20 09:24:27 +0900

    feat: ScannerReview, ScannerReviewTest 추가

---
+++ src/main/java/com/example/week1/ScannerReview.java
@@ -0,0 +1,10 @@
+package com.example.week1;
+
+import java.util.Scanner;
+
+public class ScannerReview {
+    public void readTwoNumbersAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/com/example/week1/ScannerReviewTest.java
@@ -0,0 +1,8 @@
+package com.example.week1;
+
+public class ScannerReviewTest {
+    public static void main(String[] args) {
+        ScannerReview scannerReview = new ScannerReview();
+        scannerReview.readTwoNumbersAndPlus();
+    }
+}
