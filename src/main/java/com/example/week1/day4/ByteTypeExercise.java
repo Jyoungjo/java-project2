d124e64a8bb20d89c00d4179778d0dca63b98f9a
Jyoungjo <zimbote0411@gmail.com>
2023-04-20 14:44:08 +0900

    feat: ByteTypeExercise, ScannerFormatting 추가

---
+++ src/main/java/com/example/week1/day4/ByteTypeExercise.java
@@ -0,0 +1,11 @@
+package com.example.week1.day4;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        // byte byte2 = -129;
+        // byte byte3 = 128;
+        System.out.println(byte1);
+        System.out.println(byte1);
+    }
+}
