00566cb10e6453ee3207f544294c7ba32666ca28
Jyoungjo <zimbote0411@gmail.com>
2023-04-20 13:44:10 +0900

    feat: FloatFormatEx, AverageEx 추가

---
+++ src/main/java/com/example/week1/day4/FloatFormatEx.java
@@ -0,0 +1,8 @@
+package com.example.week1.day4;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        System.out.printf("%f", 0.5f);
+        // System.out.printf("%f", 1 / 2); (X)
+    }
+}
