c1074bab001e13ba62837860b1d113791e2f9346
Jyoungjo <zimbote0411@gmail.com>
2023-04-20 11:44:30 +0900

    feat: 2장, Codeup1010 수정

--- src/main/java/com/example/codeup/Codeup1010.java
+++ src/main/java/com/example/codeup/Codeup1010.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Codeup1010 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        System.out.printf("%d", num);
+        System.out.printf("%d", sc.nextInt());
     }
 }
