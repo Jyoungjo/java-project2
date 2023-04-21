4e994770995b5564e45c45ebb60494a07b69f754
Jyoungjo <zimbote0411@gmail.com>
2023-04-21 15:25:12 +0900

    feat: User, UserTest 추가

---
+++ src/main/java/com/example/week1/day5/UserTest.java
@@ -0,0 +1,10 @@
+package com.example.week1.day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User JYJ = new User();
+        JYJ.name = "장영조";
+        JYJ.phoneNumber = "010-0000-1234";
+        JYJ.age = 26;
+    }
+}
