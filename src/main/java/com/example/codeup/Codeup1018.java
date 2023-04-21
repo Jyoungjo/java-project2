78f3fd0197d0bc518ae0f9f69e9d2c756c475cc7
Jyoungjo <zimbote0411@gmail.com>
2023-04-21 14:17:47 +0900

    feat: 2장, Codeup1018

---
+++ src/main/java/com/example/codeup/Codeup1018.java
@@ -0,0 +1,11 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String time = sc.nextLine();
+        System.out.printf("%s", time);
+    }
+}
