0676576ab8e292a4ebfce4b5b8a724319856144e
Jyoungjo <zimbote0411@gmail.com>
2023-04-24 15:15:51 +0900

    feat: 3장, Codeup1028 ~ 1030

---
+++ src/main/java/com/example/codeup/Codeup1029.java
@@ -0,0 +1,20 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%.11f", sc.nextDouble());
+    }
+}
+
+/*
+- 내가 쓴 답 -
+public class Codeup1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d", sc.nextLong());
+    }
+}
+ */
