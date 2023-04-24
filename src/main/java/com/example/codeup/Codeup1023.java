c6291eb9f38889d533e143fc332dd14c3cb6d849
Jyoungjo <zimbote0411@gmail.com>
2023-04-24 15:07:33 +0900

    feat: 3장, Codeup1021 ~ 1023

---
+++ src/main/java/com/example/codeup/Codeup1023.java
@@ -0,0 +1,15 @@
+package com.example.codeup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Codeup1023 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String[] sArr = str.split("\\.");
+
+        System.out.printf("%s\n%s", sArr[0], sArr[1]);
+    }
+}
