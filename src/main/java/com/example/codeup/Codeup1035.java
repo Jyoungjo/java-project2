13ecf1d589e831b2bbeb0d4b70a96e01391429e2
Jyoungjo <zimbote0411@gmail.com>
2023-04-25 13:34:57 +0900

    feat: 3장, Codeup1031 ~ 1037

---
+++ src/main/java/com/example/codeup/Codeup1035.java
@@ -0,0 +1,12 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1035 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.valueOf(sc.next(), 16);
+        String octaNum = Integer.toOctalString(num);
+        System.out.printf("%s", octaNum);
+    }
+}
