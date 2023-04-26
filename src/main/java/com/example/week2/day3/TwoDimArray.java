bdf0fdd981c550191c8a6c18605a4591f6cdb0ae
Jyoungjo <zimbote0411@gmail.com>
2023-04-26 11:21:57 +0900

    fix: 클래스명 축약 및 할당값 추가 - TwoDimensionalArray -> TwoDimArray

--- src/main/java/com/example/week2/day3/TwoDimArray.java
+++ src/main/java/com/example/week2/day3/TwoDimArray.java
@@ -2,9 +2,11 @@ package com.example.week2.day3;
 
 import java.util.Arrays;
 
-public class TwoDimensionalArray {
+public class TwoDimArray {
     public static void main(String[] args) {
         int[][] arr = new int [3][3];
+        int[] arr0 = arr[0];
+        arr0[0] = 1; // 값 할당
 
         System.out.println(Arrays.toString(arr[0]));
         System.out.println(Arrays.toString(arr[1]));
