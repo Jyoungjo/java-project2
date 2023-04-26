77776a1670bee5f65c9004723a7d5d5cdf109475
Jyoungjo <zimbote0411@gmail.com>
2023-04-26 10:37:30 +0900

    feat: TenxTen, TwoDimensionalArray 추가

---
+++ src/main/java/com/example/week2/day3/TwoDimensionalArray.java
@@ -0,0 +1,13 @@
+package com.example.week2.day3;
+
+import java.util.Arrays;
+
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        int[][] arr = new int [3][3];
+
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+    }
+}
