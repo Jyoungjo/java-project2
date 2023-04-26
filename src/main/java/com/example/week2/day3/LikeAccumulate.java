e7e26d545cb05ae3416909e202c5fd55d19ed022
Jyoungjo <zimbote0411@gmail.com>
2023-04-26 15:37:23 +0900

    feat: LikeAccumulate 추가

---
+++ src/main/java/com/example/week2/day3/LikeAccumulate.java
@@ -0,0 +1,27 @@
+package com.example.week2.day3;
+
+public class LikeAccumulate {
+    public static void main(String[] args) {
+        int likeCount = 0;
+
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+
+        likeCount = likeCount - 1;
+        System.out.println(likeCount);
+
+
+
+        int myAccount = 1_000_000;
+
+        myAccount = myAccount - 500_000;
+        System.out.println(myAccount);
+
+        myAccount = myAccount + 4_000_000;
+        System.out.println(myAccount);
+
+    }
+}
