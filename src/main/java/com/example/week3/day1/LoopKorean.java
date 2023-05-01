0bc4b9db1022b19fc33f6f1035ca95e5148d9154
Jyoungjo <zimbote0411@gmail.com>
2023-05-01 13:45:25 +0900

    feat: 한글 반복 출력 - LoopKorean 추가

---
+++ src/main/java/com/example/week3/day1/LoopKorean.java
@@ -0,0 +1,9 @@
+package com.example.week3.day1;
+
+public class LoopKorean {
+    public static void main(String[] args) {
+        for (char c = '가'; c <= '힣'; c++) {
+            System.out.printf("%c ", c);
+        }
+    }
+}
