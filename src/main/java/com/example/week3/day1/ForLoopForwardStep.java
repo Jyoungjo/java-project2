675eaafaa826a03206cab220e89a98841319f631
Jyoungjo <zimbote0411@gmail.com>
2023-05-01 13:16:05 +0900

    feat: 10미만의 홀수, 짝수 출력 - ForLoopForwardStep 추가

---
+++ src/main/java/com/example/week3/day1/ForLoopForwardStep.java
@@ -0,0 +1,22 @@
+package com.example.week3.day1;
+
+public class ForLoopForwardStep {
+
+    // 짝수
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
+
+    /*
+
+    // 홀수
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+
+     */
