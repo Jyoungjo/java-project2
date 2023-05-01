1387ac57204509c6c89a71ac1a4fdc326257633f
Jyoungjo <zimbote0411@gmail.com>
2023-05-01 10:50:18 +0900

    feat: for 문 사용해서 가로, 세로 숫자 출력 예제 - ForLoopPrintForward, ForLoopPrintHorizontal 추가

---
+++ src/main/java/com/example/week3/day1/ForLoopPrintForward.java
@@ -0,0 +1,9 @@
+package com.example.week3.day1;
+
+public class ForLoopPrintForward {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
