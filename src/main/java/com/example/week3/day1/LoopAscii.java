51054cc4931b39f2514da20c2a521e4f6d206df9
Jyoungjo <zimbote0411@gmail.com>
2023-05-01 13:35:00 +0900

    feat: ASCII 코드 반복 출력 방법 - LoopAscii, LoopChar, LoopCharInt 추가

---
+++ src/main/java/com/example/week3/day1/LoopAscii.java
@@ -0,0 +1,11 @@
+package com.example.week3.day1;
+
+public class LoopAscii {
+    public static void main(String[] args) {
+        for (int c = 65; c <= 'Z'; c++) {
+            System.out.printf("%d:%c ", c, c);
+        }
+        System.out.println("-------");
+        // System.out.printf("%c\n", 222222222); -> 범위 벗어나면 오류
+    }
+}
---
+++ src/main/java/com/example/week3/day1/LoopChar.java
@@ -0,0 +1,9 @@
+package com.example.week3.day1;
+
+public class LoopChar {
+    public static void main(String[] args) {
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.printf("%c ", c);
+        }
+    }
+}
