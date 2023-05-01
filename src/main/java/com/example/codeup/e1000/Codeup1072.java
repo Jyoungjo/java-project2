82fa08b377192016ea011a6ae74910a8a683d1a3
Jyoungjo <zimbote0411@gmail.com>
2023-05-01 10:51:02 +0900

    feat: Codeup1071 ~ 1072

---
+++ src/main/java/com/example/codeup/e1000/Codeup1072.java
@@ -0,0 +1,15 @@
+package com.example.codeup.e1000;
+
+import java.util.Scanner;
+
+public class Codeup1072 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            int input = sc.nextInt();
+            System.out.println(input);
+        }
+    }
+}
