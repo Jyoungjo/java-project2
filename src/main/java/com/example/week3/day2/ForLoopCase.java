4ce41770f50e83d8e34faa43dd1f490a20364460
Jyoungjo <zimbote0411@gmail.com>
2023-05-02 10:45:57 +0900

    feat: 반복문의 조건식 활용 - ForLoopCase, ForLoopCase2 추가

---
+++ src/main/java/com/example/week3/day2/ForLoopCase.java
@@ -0,0 +1,9 @@
+package com.example.week3.day2;
+
+public class ForLoopCase {
+    public static void main(String[] args) {
+        for (int i = 0; true; i++) {
+            System.out.println(i);
+        } // 조건식에 따라 무한루프가 진행될 수 있다
+    }
+}
---
+++ src/main/java/com/example/week3/day2/ForLoopCase2.java
@@ -0,0 +1,14 @@
+package com.example.week3.day2;
+
+public class ForLoopCase2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; 2 * i <= 17; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i * i <= 50; i++) {
+            System.out.println(i);
+        } // 제곱근 표현하는 방법
+    }
+}
