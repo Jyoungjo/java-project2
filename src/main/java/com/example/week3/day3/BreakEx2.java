ec308f8b3fefb60a6357fd1f66befe2d6d8e95ac
Jyoungjo <zimbote0411@gmail.com>
2023-05-03 14:14:38 +0900

    feat: 소수찾기 (break 사용) - BreakEx2, IsPrime2 추가

---
+++ src/main/java/com/example/week3/day3/BreakEx2.java
@@ -0,0 +1,10 @@
+package com.example.week3.day3;
+
+public class BreakEx2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i * i > 50) break;
+            System.out.println(i);
+        }
+    }
+}
