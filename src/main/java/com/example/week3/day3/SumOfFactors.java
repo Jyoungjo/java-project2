7afc355699ac1ebf9b52aeb7abfc44b36116e7f1
Jyoungjo <zimbote0411@gmail.com>
2023-05-03 11:47:20 +0900

    feat: 약수 합 구하기 - SumOfFactors 추가

---
+++ src/main/java/com/example/week3/day3/SumOfFactors.java
@@ -0,0 +1,11 @@
+package com.example.week3.day3;
+
+public class SumOfFactors {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) answer += i;
+        }
+        return answer;
+    }
+}
