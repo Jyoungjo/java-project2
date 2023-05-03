4194136eeff45786060d94e61795d89391602a10
Jyoungjo <zimbote0411@gmail.com>
2023-05-03 13:21:01 +0900

    feat: 소수 구하는 방법 (나누기, 루트) - IsPrimeDivide, IsPrimeRoot 추가

---
+++ src/main/java/com/example/week3/day3/IsPrimeRoot.java
@@ -0,0 +1,13 @@
+package com.example.week3.day3;
+
+public class IsPrimeRoot {
+    public static void main(String[] args) {
+        int num = 7;
+        int factors = 0;
+        for (int i = 2; i * i <= num; i++) { // 1 ~ 6 까지
+            if (num % i == 0) factors++;
+        }
+        System.out.printf("factors:%d\n", factors);
+        System.out.print(factors == 0 ? num + "은 소수입니다.": num + "은 소수가 아닙니다.");
+    }
+}
