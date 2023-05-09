7af90a5a6d05042a3ea30011eddcec059fd19893
Jyoungjo <zimbote0411@gmail.com>
2023-05-09 11:13:39 +0900

    feat: 평행사변형 만들기 (생성자 사용) - Parallelogram 추가

---
+++ src/main/java/com/example/week4/day2/Parallelogram.java
@@ -0,0 +1,20 @@
+package com.example.week4.day2;
+
+public class Parallelogram {
+    public Parallelogram(String spaceChar) {
+        this.spaceChar = spaceChar;
+    }
+
+    private String spaceChar = "0";
+
+    public String makeALine(int h, int i) {
+        return String.format("%s%s\n", spaceChar.repeat(i), "*".repeat(h));
+    }
+    public static void main(String[] args) {
+        Parallelogram parallelogram = new Parallelogram("0");
+        int h = 4;
+        for (int i = 0; i < h; i++) {
+            System.out.printf(parallelogram.makeALine(h, i));
+        }
+    }
+}
