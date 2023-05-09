a9bd458789cd74852a98b29e88a3b16d0ec00b65
Jyoungjo <zimbote0411@gmail.com>
2023-05-09 15:20:20 +0900

    feat: Abstract Class 예제 - DiEx, DiExTest, ParallelogramShapeDrawer, ParallelogramShapeDrawerTest, PyramidShapeDrawer, PyramidShapeDrawerTest, ShapeDrawer 추가

---
+++ src/main/java/com/example/week4/day2/DiExTest.java
@@ -0,0 +1,8 @@
+package com.example.week4.day2;
+
+class DiExTest {
+    public static void main(String[] args) {
+        DiEx diEx = new DiEx(new ParallelogramShapeDrawer());
+        diEx.doSth();
+    }
+}
