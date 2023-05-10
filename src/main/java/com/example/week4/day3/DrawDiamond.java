c02365bf76519f541e68303db06ada3e9cf6ef0d
Jyoungjo <zimbote0411@gmail.com>
2023-05-10 10:23:37 +0900

    fix: repeat() 사용하지 않은 메소드 추가

--- src/main/java/com/example/week4/day3/DrawDiamond.java
+++ src/main/java/com/example/week4/day3/DrawDiamond.java
@@ -5,14 +5,24 @@ public class DrawDiamond {
         return symbol.repeat(n);
     }
 
+    public static String getRepeatSymbolFor(String symbol, int n) {
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result += symbol;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int h = 7;
         int pivot = h / 2;
         for (int i = 0; i < h; i++) {
             if (i <= pivot) {
-                System.out.printf("%s%s\n", getRepeatSymbol(" ", pivot - i), getRepeatSymbol("*", 2 * i + 1));
+                // repeat(): System.out.printf("%s%s\n", getRepeatSymbol(" ", pivot - i), getRepeatSymbol("*", 2 * i + 1));
+                System.out.printf("%s%s\n", getRepeatSymbolFor(" ", pivot - i), getRepeatSymbolFor("*", 2 * i + 1));
             } else {
-                System.out.printf("%s%s\n", getRepeatSymbol(" ", i - pivot), getRepeatSymbol("*", 2 * (h - i) - 1));
+                // repeat(): System.out.printf("%s%s\n", getRepeatSymbol(" ", i - pivot), getRepeatSymbol("*", 2 * (h - i) - 1));
+                System.out.printf("%s%s\n", getRepeatSymbolFor(" ", i - pivot), getRepeatSymbolFor("*", 2 * (h - i) - 1));
             }
         }
     }
