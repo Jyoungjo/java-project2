61e0f1eec9553440ecea1cc6b63c23c7ea85e8c5
Jyoungjo <zimbote0411@gmail.com>
2023-05-15 10:53:24 +0900

    fix: Constructor, Getter, Setter 추가

--- src/main/java/com/example/week4/day4/Address.java
+++ src/main/java/com/example/week4/day4/Address.java
@@ -1,4 +1,25 @@
 package com.example.week4.day4;
 
 public class Address {
+    private String fullAddr;
+    private String side;
+    private String sigungu;
+
+    public Address(String fullAddr, String side, String sigungu) {
+        this.fullAddr = fullAddr;
+        this.side = side;
+        this.sigungu = sigungu;
+    }
+
+    public String getFullAddr() {
+        return fullAddr;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public String getSigungu() {
+        return sigungu;
+    }
 }
--- src/main/java/com/example/week4/day4/ReadFileEx.java
+++ src/main/java/com/example/week4/day4/ReadFileEx.java
@@ -1,13 +1,62 @@
 package com.example.week4.day4;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import com.example.week4.day5.LineReader;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ReadFileEx {
+    LineReader lineReader;
+    Charset charset;
+
+    public ReadFileEx(Charset charset) {
+        this.charset = charset;
+    }
+
+    public List<String> getLines(String fileName) throws IOException {
+        List<String> lines = new LinkedList<>();
+
+        // BufferedReader선언 --> File연결
+        BufferedReader br = new BufferedReader(new FileReader(fileName, charset));
+
+        // BufferedReader에서 loop으로 한줄씩 불러오기
+        String line;
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+        }
+        return lines;
+    }
+
+    public Hospital parse(String str) {
+        System.out.println(str);
+        String[] splitted = str.split(",");
+        Address address = new Address(splitted[10], splitted[5], splitted[7]);
+        Hospital hospital = new Hospital(splitted[1], splitted[11], address);
+
+        return hospital;
+    }
+
+    public List<Hospital> getHospitals(List<String> lines) {
+        List<Hospital> hospitals = new ArrayList<>();
+        for (String line : lines) {
+            hospitals.add(parse(line));
+        }
+        return hospitals;
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\JOOYON\\Desktop\\건강보험심사평가원_전국 병의원 및 약국 현황 2022.9\\1.병원정보서비스 2022.10..csv"));
-        System.out.println(br.readLine());
+        ReadFileEx rfe = new ReadFileEx(Charset.forName("UTF-8"));
+        List<String> strLines = rfe.getLines("hospital_info_10lines.csv");
+        List<Hospital> parsedHospital = rfe.getHospitals(strLines);
+        for (int i = 0; i < parsedHospital.size(); i++) {
+            Hospital hospital = parsedHospital.get(i);
+            System.out.printf("%s %s %s\n", hospital.getName(), hospital.getWebsiteAddress(),
+                    hospital.getAddress().getFullAddr());
+        }
+
     }
 }
