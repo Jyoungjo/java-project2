f2507b01a41abd5b6f17674b87f87151fe8bfe34
Jyoungjo <zimbote0411@gmail.com>
2023-05-18 10:25:23 +0900

    feat: Stack - Stack1 추가

---
+++ src/main/java/com/example/week5/day3/Stack1.java
@@ -0,0 +1,36 @@
+package com.example.week5.day3;
+
+import java.util.Arrays;
+
+public class Stack1 {
+
+    private int[] arr = new int[10000];
+    private int pointer = 0;
+
+
+    public void push(int value) {
+        this.arr[pointer++] = value;
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println("pointer = " + pointer);
+    }
+
+
+    public boolean isEmpty() {
+        return this.pointer == 0;
+    }
+
+    public int pop() {
+        if (isEmpty()) throw new RuntimeException("스택이 비었습니다");
+        return this.arr[--pointer];
+    }
+
+    public int peek() {
+        if (isEmpty()) throw new RuntimeException("스택이 비었습니다");
+        return this.arr[pointer - 1];
+    }
+
+    public static void main(String[] args) {
+        Stack1 st = new Stack1();
+        System.out.println(st.peek());
+    }
+}
